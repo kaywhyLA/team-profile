7a07adea81064121a8480ecc50c8035ff3972693
KHAYLLA HITE <KHAYLLAHOUSTON@YAHOO.COM>
2022-08-02 09:38:41 -0500

    created manager object and manager getRole object, passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -11,13 +11,13 @@ test('create employee id?', () => {
     const employee = new Employee('');
 
     expect(employee.id).toBe('10171738');
-})
+});
 
 test('create employee email?', () => {
     const employee = new Employee('');
 
     expect(employee.email).toBe('jhouston@yahoo.com');
-})
+});
 
 
 module.exports = Employee;
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,13 @@
+const Manager = require('../lib/Manager');
+const manager = new Manager('10171738', 'jhouston@yahoo.com');
+
+test('create manager object', () => {
+    const manager = new Manager('');
+
+    expect(manager.id).toBe('10171738');
+    expect(manager.email).toBe('jhouston@yahoo.com');
+});
+
+test('create getRole object', () => {
+    expect(manager.getRole()).toBe('Manager');
+})
